f8f43ccfe85ba1e182e13e6994b6d9eb581fb3f4
Toshko Todorov <74822232+toshko-mnt@users.noreply.github.com>
2021-07-10 00:24:08 +0300

    add validation

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ var form = document.getElementsByTagName('form')[0],
 	listAll = document.getElementsByTagName('ul')[0],
 	btnResult = document.getElementsByTagName('input')[2],
 	resultCount = document.getElementsByTagName('h2')[1],
-	arrIngredient = [], AllPermutator = [], magicalPermutator = [],
+	error1 = document.getElementsByTagName('p')[0],
+	error2 = document.getElementsByTagName('p')[1],
+	arrMagical = [], AllPermutator = [], magicalPermutator = [],
 	tmpArr, magicalAction, listIngredient;
 
 function permutator(inputArr) {
@@ -26,20 +28,33 @@ function permutator(inputArr) {
 
 form.addEventListener('submit', function(e){
 	e.preventDefault();
+	
+	if(ingredient.value.length >= 1 && ingredient.value.length < 20 ){
+		error1.style.display = 'none';
+		tmpArr = ingredient.value.split(',');
+		if(tmpArr.length == 2){
+			error2.style.display = 'none';
+			magicalAction = tmpArr[1].trim();
+			arrMagical.push(magicalAction);
 
-	tmpArr = ingredient.value.split(',',2);
-	magicalAction = tmpArr[1];
-	arrIngredient.push(magicalAction);
-
-	listIngredient = document.createElement('li');
-	listIngredient.innerText = ingredient.value;
-	listAll.appendChild(listIngredient);
-	btnResult.style.display = 'block';
+			listIngredient = document.createElement('li');
+			listIngredient.innerText = ingredient.value;
+			listAll.appendChild(listIngredient);
+			btnResult.style.display = 'block';
+		}else{
+			error2.style.color = 'red';
+			error2.style.display = 'block';
+		}
+	}else{
+		error1.style.color = 'red';
+		error1.style.display = 'block';
+		error2.style.display = 'none';
+	}
 }, false);
 
 btnResult.addEventListener('click', function(e){
 
-	AllPermutator = permutator(arrIngredient);
+	AllPermutator = permutator(arrMagical);
 	//console.log(AllPermutator);
 	for(let i = 0; i < AllPermutator.length; i++){
 		flag = 1;
