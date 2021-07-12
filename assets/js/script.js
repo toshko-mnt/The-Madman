e9faf89b8fa1652da60244ad37e30a7fb234c5fe
Toshko Todorov <74822232+toshko-mnt@users.noreply.github.com>
2021-07-12 16:56:25 +0300

    move external files to a folder assets

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+
+var form = document.getElementsByTagName('form')[0],
+	ingredient = document.getElementsByTagName('input')[0],
+	listAll = document.getElementsByTagName('ul')[0],
+	btnResult = document.getElementsByTagName('input')[2],
+	resultCount = document.getElementsByTagName('h2')[1],
+	imgElement = document.getElementsByTagName('img')[0],
+	error = document.getElementsByTagName('p')[0],
+	arrMagical = [], AllPermutator = [], magicalPermutator = [],
+	tmpArr, magicalAction, listIngredient;
+
+function permutator(inputArr) {
+	let results = [];
+	function permute(arr, memo) {
+		var cur, memo = memo || [];
+		for (let i = 0; i < arr.length; i++) {
+			cur = arr.splice(i, 1);
+			if (arr.length === 0) {
+				results.push(memo.concat(cur));
+			}
+			permute(arr.slice(), memo.concat(cur));
+			arr.splice(i, 0, cur[0]);
+		}
+		return results;
+	}
+	return permute(inputArr);
+}
+
+form.addEventListener('submit', function(e){
+	e.preventDefault();
+	
+	if(ingredient.value.length >= 1 && ingredient.value.length < 20 ){
+		error.style.display = 'none';
+		tmpArr = ingredient.value.split(',');
+		if(tmpArr.length == 2){
+			listAll.children[0].style.display = 'none';
+			magicalAction = tmpArr[1].trim();
+			if(magicalAction.length > 0){
+				arrMagical.push(magicalAction);
+				listIngredient = document.createElement('li');
+				listIngredient.innerText = ingredient.value;
+				listAll.appendChild(listIngredient);
+				btnResult.style.display = 'block';
+			}
+		}else{
+			error.innerText = 'Невалидни данни';
+			error.style.display = 'block';
+		}
+	}else{
+		error.innerText = '1 ≤ ingredients.length < 20';
+		error.style.display = 'block';
+	}
+}, false);
+
+btnResult.addEventListener('click', function(e){
+
+	AllPermutator = permutator(arrMagical);
+	//console.log(AllPermutator);
+	for(let i = 0; i < AllPermutator.length; i++){
+		flag = 1;
+		for(let x = 0; x < AllPermutator[i].length - 1; x++){
+			if(AllPermutator[i][x] == AllPermutator[i][x + 1]){
+				flag = 2;	
+			}
+		}
+		if(flag == 1){
+			magicalPermutator.push(AllPermutator[i]);
+		}
+	}
+	//console.log(magicalPermutator);
+	resultCount.innerText = 'Опазихте света за ' + magicalPermutator.length + ' дни!';
+	if(magicalPermutator.length > 0){
+		imgElement.setAttribute('src' , 'assets/img/logo1.jpg');
+	}else{
+		imgElement.setAttribute('src' , 'assets/img/logo2.jpg');
+	}
+	magicalPermutator = [];
+}, false);
+
+
